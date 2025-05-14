b4942f05fa5c92f7da9f2afd036bc24582066f3a
thiego <projetoxideias@gmail.com>
2025-05-14 14:05:04 -0300

    logica de programacao01

---
+++ 01-numbers.py
@@ -0,0 +1,34 @@
+idade = 25
+print(idade)
+
+#utilize '#' para criar comentários sem afetar a execução do código 
+
+idade1 = 1
+idade_2 = 2
+#nome inválido -> age-3 = 3
+#nome inválido -> 4age = 4
+idade_de_usuários = 7 #recomendado underline para múltiplas palavras baseado no seguinte guia:
+#https://www.python.org/dev/peps/pep-0008/#function-and-variable-names
+
+#continue = 5 --> proibido atribuir nome de palavras-chave 
+
+########## OPERADORES ##########
+resultado = 20 / 3 #6.6666....
+print(resultado) 
+
+print("para converter em decimal utilize: ", 20 // 3) #6
+
+resultado = 5**2 #elevando um número a uma potência (5^)
+print(resultado)
+
+#voce pode utilizar a função Round para arredondar para cima 
+print(round(20/3), 0)
+
+print(resultado / idade) #Voce pode também utilizar variáveis para a mesma expressão lógica.
+
+#a regra de precedencia matemática é aplicada também nas expressões lógicas
+print((20 / 3) + 1)
+
+#voce pode definir a prioridade de execução utilizado ()
+print(20 / (3 + 1))
+
